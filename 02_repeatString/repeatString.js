e73cf7f3533800f6c228bea424dae9dfc882975f
drebob24 <abobbit@gmail.com>
2023-05-15 18:19:57 +0300

    Update previous lessons
    No special notes. Tests were straightforward.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        let concatString = "";
+        for (let i = 1; i <= num; i++){
+            concatString += str;
+        }
+        return concatString;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringLetters = string.split("");
+    let stringReversed = "";
+    for (const letters of stringLetters){
+        stringReversed = letters + stringReversed;
+    }
+    return stringReversed
 };
 
 // Do not edit below this line
