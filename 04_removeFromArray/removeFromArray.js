ee8c49a931b48d103c156d8f0d5e94b3d887a0a6
drebob24 <abobbit@gmail.com>
2023-05-15 17:45:10 +0300

    Create function to remove multiple Array values
    Lesson 4, Test 2 requires deleting multiple values from an array.
    This required learning how to accept an unknown amount of values into
    a function. Handled this using a rest parameter, then filtering through
    the original array looping through the values of the created
    rest parameter array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    for (const toDelete of args){
 
+        array = array.filter(function(arrayContent) {
+            return arrayContent !== toDelete;
+        });
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
